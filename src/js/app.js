06603da3c98ca4873f3863b5a8fa3e61c39b83a3
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-08-02 00:38:56 +0100

    Style Search Form

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,8 @@ var App = React.createClass({
   render: function () {
     return (
       <div className='container-fluid jumbotron header'>
-        <a className='logo'>Git Releases</a>
+        <h1 className='logo'>Git Releases</h1>
+        <h3 className='desc'>a nice way to view repositories GitHub Releases</h3>
         <SearchForm />
       </div>
     );
--- src/js/components/search-form.js
+++ src/js/components/search-form.js
@@ -3,19 +3,19 @@ var React = require('react');
 var SearchForm = React.createClass({
   render: function () {
     return (
-      <div className='container'>
+      <div className='container search-form'>
         <div className='row'>
 
-          <div className='col-xs-offset-2 col-xs-3'>
+          <div className='col-md-offset-2 col-md-3'>
             <input className="form-control input-lg" type="text" placeholder="username" />
           </div>
 
-          <div className='col-xs-3'>
+          <div className='col-md-4'>
             <input className="form-control input-lg" type="text" placeholder="repository" />
           </div>
 
-          <div className='col-xs-3'>
-            <button className='btn btn-lg'>Show me some releases</button>
+          <div className='col-md-1'>
+            <button className='btn btn-lg btn-default btn-block'>Go!</button>
           </div>
 
         </div>
